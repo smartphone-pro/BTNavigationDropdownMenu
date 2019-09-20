f7cc737f08aa61adf68af40b68ca5301d4c690ae
smartpro <smartphone_pro@hotmail.com>
2019-09-20 21:06:37 +0700

    Change resource

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -17,9 +17,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         let items = ["Most Popular", "Latest", "Trending", "Nearest", "Top Picks"]
         self.selectedCellLabel.text = items.first
-        self.navigationController?.navigationBar.isTranslucent = false
-        self.navigationController?.navigationBar.barTintColor = UIColor(red: 0.0/255.0, green: 180/255.0, blue: 220/255.0, alpha: 1.0)
-        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+//        self.navigationController?.navigationBar.isTranslucent = false
+//        self.navigationController?.navigationBar.barTintColor = UIColor(red: 0.0/255.0, green: 180/255.0, blue: 220/255.0, alpha: 1.0)
+//        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
 
         // "Old" version
         // menuView = BTNavigationDropdownMenu(navigationController: self.navigationController, containerView: self.navigationController!.view, title: "Dropdown Menu", items: items)
@@ -31,7 +31,7 @@ class ViewController: UIViewController {
 
         menuView.cellHeight = 50
         menuView.cellBackgroundColor = self.navigationController?.navigationBar.barTintColor
-        menuView.cellSelectionColor = UIColor(red: 0.0/255.0, green: 160.0/255.0, blue: 195.0/255.0, alpha: 1.0)
+        // menuView.cellSelectionColor = UIColor(red: 0.0/255.0, green: 160.0/255.0, blue: 195.0/255.0, alpha: 1.0)
         menuView.shouldKeepSelectedCellColor = true
         menuView.cellTextLabelColor = UIColor.white
         menuView.cellTextLabelFont = UIFont(name: "Avenir-Heavy", size: 17)
